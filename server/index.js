2ecb3fbbf358b2206d03c9bfef68b76adc8e439f
ssingh653 <souravsingh653@gmail.com>
2023-11-07 18:37:41 +0530

    added tailwind config

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 const connectDB = require("./config/dbConn");
 
 require("dotenv").config();
