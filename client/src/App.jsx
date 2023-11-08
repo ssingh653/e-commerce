1d8d6a9521494982921c05d409ffd3a5297200e0
ssingh653 <souravsingh653@gmail.com>
2023-11-08 15:44:10 +0530

    added signup,login and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
-import React from 'react'
-import {BrowserRouter} from "react-router-dom"
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import Routing from "./components/Routes/Routing";
 import Topbar from "./components/Topbar/Topbar";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Topbar />
-      <Routing />
-      {/* <h1 className="border m-4">E-Commerce</h1> */}
+        <Topbar />
+        <Routing />
+        <Footer />
       </BrowserRouter>
     </div>
   );
