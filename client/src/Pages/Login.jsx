ab7ef9e48d4a77e7f3164a32511830d298b6d7ad
ssingh653 <souravsingh653@gmail.com>
2023-11-08 19:55:04 +0530

    minor changes

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -13,17 +13,9 @@ import { NavLink } from "react-router-dom";
   }
   ```
 */
-export default function Example() {
+export default function Login() {
   return (
     <>
-      {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,6 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    // require('@tailwindcss/aspect-ratio'),
+    //require("@tailwindcss/forms")
   ],
 };
